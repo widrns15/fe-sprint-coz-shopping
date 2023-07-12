58e33c70f7c46ef4859e90c44f8e1661a282bd70
widrns15 <85780501+widrns15@users.noreply.github.com>
2023-07-12 15:36:55 +0900

    🔥 feat: 페이지 생성 및 라우팅 적용

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
+import React from "react";
+import BookMarkPage from "./pages/BookMarkPage";
+import MainPage from "./pages/MainPage";
+import ProductListPage from "./pages/ProductListPage";
+import { HashRouter, Routes, Route, Link } from "react-router-dom";
 import "./App.css";
 
 function App() {
-  return <div className="App">개발 초기 환경 리팩토링</div>;
+  return (
+    <HashRouter>
+      <Routes>
+        <Route path="/" element={<MainPage />} />
+        <Route path="/bookmark" element={<BookMarkPage />} />
+        <Route path="/products/list" element={<ProductListPage />} />
+      </Routes>
+    </HashRouter>
+  );
 }
 
 export default App;
---
+++ src/pages/BookMarkPage.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const BookMarkPage = () => {
+  return <div>BookMarkPage</div>;
+};
+
+export default BookMarkPage;
