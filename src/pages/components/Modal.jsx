d1b26425e82e5e0ccb45943f0d3bd7f716d1a3ac
widrns15 <85780501+widrns15@users.noreply.github.com>
2023-07-17 04:20:41 +0900

    🔥 feat: Modal 컴포넌트 & 기능 추가

---
+++ src/pages/components/Modal.jsx
@@ -0,0 +1,95 @@
+import React from "react";
+import styled from "styled-components";
+import markStar from "../img/markstar.svg";
+import unMarkStar from "../img/unmarkstar.svg";
+
+const Modal = ({ item, isMarked, handleModalClose, bookMarkHandler }) => {
+  const backgroundImage = item.brand_image_url
+    ? `url(${item.brand_image_url})`
+    : `url(${item.image_url})`;
+
+  const closeModal = (e) => {
+    e.stopPropagation();
+    handleModalClose();
+  };
+
+  const toggleBookmark = () => {
+    bookMarkHandler();
+  };
+
+  const getItemTitle = () => {
+    if (item.type === "Category") {
+      return `# ${item.title}`;
+    }
+    return item.title || item.brand_name;
+  };
+
+  return (
+    <ModalSection>
+      <div className="background" style={{ backgroundImage }}>
+        <button className="closebutton" onClick={closeModal}>
+          X
+        </button>
+        <div className="description">
+          <img
+            onClick={toggleBookmark}
+            className="bookmark"
+            src={isMarked ? markStar : unMarkStar}
+            alt="bookmark"
+          />
+          <span>{getItemTitle()}</span>
+        </div>
+      </div>
+    </ModalSection>
+  );
+};
+
+const ModalSection = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100vw;
+  height: 100vh;
+  background: rgba(255, 255, 255, 0.4);
+  color: white;
+  cursor: grab;
+  z-index: 1;
+
+  .background {
+    position: absolute;
+    background-size: cover;
+    background-position: center;
+    border-radius: 0.75rem;
+    top: 50%;
+    left: 50%;
+    width: 46.5rem;
+    height: 30rem;
+    transform: translate(-50%, -50%);
+  }
+
+  .closebutton {
+    position: absolute;
+    right: 1.5rem;
+    top: 1.5rem;
+    color: white;
+    background-color: transparent;
+    cursor: pointer;
+  }
+
+  .bookmark {
+    cursor: pointer;
+  }
+
+  .description {
+    position: absolute;
+    display: flex;
+    align-items: center;
+    bottom: 1.5rem;
+    left: 1.5rem;
+    gap: 4px;
+    font-size: 1.5rem;
+    font-weight: 700;
+  }
+`;
+
+export default Modal;
--- src/pages/components/ProductCard.jsx
+++ src/pages/components/ProductCard.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import styled from "styled-components";
 import markStar from "../img/markstar.svg";
 import unMarkStar from "../img/unmarkstar.svg";
+import Modal from "./Modal";
 
 const ProductCard = ({ item }) => {
   const {
@@ -18,9 +19,18 @@ const ProductCard = ({ item }) => {
   } = item;
 
   const [isMarked, setIsMarked] = useState(item.marked);
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
   const bookMarkHandler = () => {
-    setIsMarked(!isMarked);
+    setIsMarked((prevMarked) => !prevMarked);
+  };
+
+  const handleCardClick = () => {
+    setIsModalOpen(true);
+  };
+
+  const handleModalClose = () => {
+    setIsModalOpen(false);
   };
 
   switch (type) {
@@ -28,81 +38,125 @@ const ProductCard = ({ item }) => {
       return (
         <CardSection>
           <div className="thumbnail">
-            <img src={image_url} alt="thumbnail" />
             <img
               className="bookmark"
               onClick={bookMarkHandler}
               src={isMarked ? markStar : unMarkStar}
               alt="bookmark"
             />
+            <img src={image_url} alt="thumbnail" onClick={handleCardClick} />
           </div>
-          <div>
-            <span className="title">{title}</span>
-            <span className="discount">{discountPercentage}%</span>
-          </div>
-          <div>
-            <span></span>
-            <span className="price">{price}원</span>
-          </div>
+          <DescriptionSection onClick={handleCardClick}>
+            <div>
+              <span className="title">{title}</span>
+              <span className="discount">{discountPercentage}%</span>
+            </div>
+            <div>
+              <span></span>
+              <span className="price">{price}원</span>
+            </div>
+          </DescriptionSection>
+          {isModalOpen && (
+            <Modal
+              item={item}
+              isMarked={isMarked}
+              handleModalClose={handleModalClose}
+              bookMarkHandler={bookMarkHandler}
+            />
+          )}
         </CardSection>
       );
     case "Category":
       return (
         <CardSection>
           <div className="thumbnail">
-            <img src={image_url} alt="thumbnail" />
             <img
               className="bookmark"
               onClick={bookMarkHandler}
               src={isMarked ? markStar : unMarkStar}
               alt="bookmark"
             />
+            <img src={image_url} alt="thumbnail" onClick={handleCardClick} />
           </div>
-          <div>
-            <span className="title"># {title}</span>
-          </div>
+          <DescriptionSection onClick={handleCardClick}>
+            <div>
+              <span className="title"># {title}</span>
+            </div>
+          </DescriptionSection>
+          {isModalOpen && (
+            <Modal
+              item={item}
+              isMarked={isMarked}
+              handleModalClose={handleModalClose}
+              bookMarkHandler={bookMarkHandler}
+            />
+          )}
         </CardSection>
       );
     case "Exhibition":
       return (
         <CardSection>
           <div className="thumbnail">
-            <img src={image_url} alt="thumbnail" />
             <img
               className="bookmark"
               onClick={bookMarkHandler}
               src={isMarked ? markStar : unMarkStar}
               alt="bookmark"
             />
+            <img src={image_url} alt="thumbnail" onClick={handleCardClick} />
           </div>
-          <div>
-            <span className="title">{title}</span>
-          </div>
-          <div>
-            <span className="sub_title">{sub_title}</span>
-          </div>
+          <DescriptionSection onClick={handleCardClick}>
+            <div>
+              <span className="title">{title}</span>
+            </div>
+            <div>
+              <span className="sub_title">{sub_title}</span>
+            </div>
+          </DescriptionSection>
+          {isModalOpen && (
+            <Modal
+              item={item}
+              isMarked={isMarked}
+              handleModalClose={handleModalClose}
+              bookMarkHandler={bookMarkHandler}
+            />
+          )}
         </CardSection>
       );
     case "Brand":
       return (
         <CardSection>
           <div className="thumbnail">
-            <img src={brand_image_url} alt="thumbnail" />
             <img
               className="bookmark"
               onClick={bookMarkHandler}
               src={isMarked ? markStar : unMarkStar}
               alt="bookmark"
             />
+            <img
+              src={brand_image_url}
+              alt="thumbnail"
+              onClick={handleCardClick}
+            />
           </div>
-          <div>
-            <span className="title">{brand_name}</span>
-            <span className="follower">관심고객수</span>
-          </div>
-          <div>
-            <span></span>
-            <span className="follower">{follower}</span>
-          </div>
+          <DescriptionSection onClick={handleCardClick}>
+            <div>
+              <span className="title">{brand_name}</span>
+              <span className="follower">관심고객수</span>
+            </div>
+            <div>
+              <span></span>
+              <span className="follower">{follower}</span>
+            </div>
+          </DescriptionSection>
+          {isModalOpen && (
+            <Modal
+              item={item}
+              isMarked={isMarked}
+              handleModalClose={handleModalClose}
+              bookMarkHandler={bookMarkHandler}
+            />
+          )}
         </CardSection>
       );
     default:
@@ -132,9 +186,12 @@ const CardSection = styled.div`
       cursor: pointer;
     }
   }
+`;
+
+const DescriptionSection = styled.div`
   div {
     display: flex;
-    margin-top: 6px;
+    margin-top: 0.375rem;
   }
   span:first-child {
     flex: 1;
